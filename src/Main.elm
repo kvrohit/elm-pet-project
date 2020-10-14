e02ddd4b63a6e970d6632a21ac271891cc43f185
Rohit K Viswanath <kvrohit@gmail.com>
2020-10-14 15:52:21 +0530

    Init

---
+++ src/Main.elm
@@ -0,0 +1,11 @@
+module Main exposing (main)
+
+import Html exposing (h1, text)
+
+
+
+-- MAIN
+
+
+main =
+    h1 [] [ text "Hello from Elm!" ]
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.elm'
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
